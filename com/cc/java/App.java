ffef96d7bb68bd46e91c1ccc519f9416ddec97a4
Peter Schmidt <schmidt.multimedia@gmail.com>
2025-01-24 11:07:57 +0100

    Add files via upload

---
+++ com/cc/java/App.java
@@ -0,0 +1,36 @@
+package com.cc.java;
+
+public class App {
+    
+    public static void main(String[] args) {
+     
+
+        Queen queen = new Queen();
+        Worker worker = new Worker();
+        Drone drone = new Drone(); 
+
+        pollObj(queen);
+        pollObj(worker);
+        pollObj(drone);
+
+         //-- Das sollte eigentlich nicht gehen!--//
+        HoneyBee bee = new HoneyBee(); // SuperKlasse!
+        pollObj(bee);
+        
+    }
+
+    // 2. Iteration: Polymorphie
+    private static void pollObj(HoneyBee obj){
+        output(obj.doYourJob());
+        output(obj.fly());
+        output("-------------------");
+    }
+
+
+    private static void output(String outputStr) {
+        System.out.println(outputStr);
+    }
+
+
+}
+
